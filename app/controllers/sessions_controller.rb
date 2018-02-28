200bccb5b388fa93e30b26348e85ec9ee5d365b1
Nipun <mangal.rajat98@gmail.com>
2018-02-28 19:43:44 +0000

    checking 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if (user && user.authenticate(params[:session][:password]))
+    if (user )
       session[:user_id] = user.id
       redirect_to root_path
     else
@@ -21,3 +21,5 @@ class SessionsController < ApplicationController
   end
   
 end
+
+#&& user.authenticate(params[:session][:password])
