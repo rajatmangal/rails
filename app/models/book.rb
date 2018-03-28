5e4abf62ab8fc85d88a747dd95a6af53264c0187
agam2091995 <agam2091995@gmail.com>
2018-03-27 21:24:13 -0400

    Added isbn validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>",thumb: "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :ISBN, presence: true, 
-            uniqueness: true
+            :isbn_format => true
   validates :name, presence: true
   validates :author, presence: true
   validates :user_id, presence: true
