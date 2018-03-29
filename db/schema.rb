4c7a16c56233d1913db4b07f66dd88d650178f41
Rajat <mangal.rajat98@gmail.com>
2018-03-28 23:10:38 -0400

    Google map api fixed

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,9 @@ ActiveRecord::Schema.define(version: 20180329024331) do
   end
 
   create_table "wish_lists", force: :cascade do |t|
-    t.string   "name"
-    t.string   "ISBN"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.string  "name"
+    t.string  "ISBN"
+    t.integer "user_id"
   end
 
 end
