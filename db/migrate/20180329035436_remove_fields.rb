e5cd0f90cc0777ac3a647c99f4884049705993c0
Nipun <mangal.rajat98@gmail.com>
2018-03-29 04:09:30 +0000

    Heroku error 7

--- db/migrate/20180329035436_remove_fields.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFields < ActiveRecord::Migration
-  def change
-    remove_column :books, :description
-    remove_column :books, :price
-    remove_column :books, :availability
-    remove_column :books, :created_at
-    remove_column :books, :updated_at
-  end
-end
